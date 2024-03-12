28eb02e4b5c58bfb4afbe491646456011f4e4771
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2024-03-12 12:58:47 +0200

    Minor changes

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -41,6 +41,7 @@ main = do
          Left errMsg -> putStrLn errMsg
          Right _    -> return ()
 
+-- TODO write tests
 doEditVault :: Substrate.Substrate m => VaultInfo -> ExceptT String m ()
 doEditVault vi = do
     vri <- openVault $ (localname vi) ++ ".vault"
@@ -49,12 +50,15 @@ doEditVault vi = do
         `catchError` (\e -> closeVault vri >> throwError e)
     closeVault vri
 
+-- TODO write tests
 doUploadVault :: Substrate.Substrate m => VaultInfo -> ExceptT String m ()
 doUploadVault vi = uploadVaultPartition vi (localname vi)
 
+-- TODO write tests
 doDownloadVault :: Substrate.Substrate m => VaultInfo -> ExceptT String m ()
 doDownloadVault vi = mapM_ (downloadVaultPartition vi) (remotes vi)
 
+-- TODO write tests
 doSyncVault :: Substrate.Substrate m => VaultInfo -> FilePath -> ExceptT String m ()
 doSyncVault vi remote = do
     remoteVRI <- openVault $ remote ++ ".vault"
@@ -62,6 +66,7 @@ doSyncVault vi remote = do
         `catchError` (\e -> closeVault remoteVRI >> throwError e)
     closeVault remoteVRI
 
+-- TODO write tests
 syncLocalPartition :: Substrate.Substrate m => VaultInfo -> VaultRuntimeInfo -> FilePath -> ExceptT String m ()
 syncLocalPartition vi remoteVRI remote = do
     localVRI <- openVault $ (localname vi) ++ ".vault"
@@ -69,6 +74,7 @@ syncLocalPartition vi remoteVRI remote = do
         `catchError` (\e -> closeVault localVRI >> throwError e)
     closeVault localVRI
 
+-- TODO write tests
 performSync :: Substrate.Substrate m => VaultRuntimeInfo -> FilePath -> ExceptT String m ()
 performSync localVRI remote = do
     lift $ Substrate.changeDir (repositoryDir localVRI)
@@ -77,19 +83,23 @@ performSync localVRI remote = do
 doError :: Substrate.Substrate m => String -> VaultInfo -> ExceptT String m ()
 doError msg _ = throwError msg
 
+-- TODO write tests
 uploadVaultPartition :: Substrate.Substrate m => VaultInfo -> FilePath -> ExceptT String m ()
 uploadVaultPartition vi partition =
     mapM_ (upload vi) [partition ++ ".vault", partition ++ ".log"]
 
+-- TODO write tests
 downloadVaultPartition :: Substrate.Substrate m => VaultInfo -> FilePath -> ExceptT String m ()
 downloadVaultPartition vi partition = do
     mapM_ (download vi) [partition ++ ".vault", partition ++ ".log"]
 
+-- TODO write tests
 upload :: Substrate.Substrate m => VaultInfo -> FilePath -> ExceptT String m ()
 upload vi filename = do
     let remoteFilename = mkpath [(remoteStore vi), (name vi), filename]
     lift $ Substrate.call "rsync" ["-ivz", filename, remoteFilename]
 
+-- TODO write tests
 download :: Substrate.Substrate m => VaultInfo -> FilePath -> ExceptT String m ()
 download vi filename = do
     let remoteFilename = mkpath [(remoteStore vi), (name vi), filename]
