1533914489de260990d692986a9e062a789abdf6
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2025-07-02 00:46:00 +0300

    Implement initial functionality and tests for f:verifyRepo

--- lib/Vaults/Repo.hs
+++ lib/Vaults/Repo.hs
@@ -1,13 +1,38 @@
 module Vaults.Repo where
 
-data RepoVerification
+import Control.Monad.Except
+import Control.Monad.Trans
+import System.Exit
+import qualified Vaults.Substrate as Substrate
+
+data RepoIssue
   = MissingRepoDir
   | UninitializedGit
   | MissingGitRemotes [GitRemote]
   | MissingGitSafeDirs [GitRemote]
-  | ConformalRepo
+  deriving (Eq, Show)
 
 data GitRemote = GitRemote
   { remoteName :: String,
     remoteURL :: String
   }
+  deriving (Eq, Show)
+
+verifyRepo :: (Substrate.Substrate m) => ExceptT RepoIssue m ()
+verifyRepo = do
+  checkRepoDir
+  checkGitInitialized
+
+checkRepoDir :: (Substrate.Substrate m) => ExceptT RepoIssue m ()
+checkRepoDir = do
+  exists <- lift $ Substrate.dirExists "repo"
+  if (not exists)
+    then throwError MissingRepoDir
+    else return ()
+
+checkGitInitialized :: (Substrate.Substrate m) => ExceptT RepoIssue m ()
+checkGitInitialized = do
+  result <- lift $ Substrate.exec "git" ["status"] ""
+  if (Substrate.exitCode result) /= ExitSuccess
+    then throwError UninitializedGit
+    else return ()
--- test/TestRepo.hs
+++ test/TestRepo.hs
@@ -1,10 +1,62 @@
 module TestRepo where
 
+import Assertions
+import Control.Monad.Except
+import Control.Monad.State
+import Control.Monad.Trans
+import qualified DummyValues as D
+import MockSubstrate
 import Test.HUnit
+import Vaults.Repo
 
 allTests :: Test
-allTests = TestList [test_MissingRepoDir]
+allTests =
+  TestList
+    [ test_MissingRepoDir,
+      test_UninitializedGit
+    ]
 
 test_MissingRepoDir :: Test
-test_MissingRepoDir = TestCase $ do
-  assertFailure "not implemented"
+test_MissingRepoDir =
+  TestCase $ do
+    let mock = emptyMock
+    let result = runState (runExceptT verifyRepo) mock
+    let mockAfterExec = snd result
+
+    let expectedCommands = [("dirExists", ["repo"])]
+
+    assertEqualLists
+      "check repo commands"
+      expectedCommands
+      (execRecorded mockAfterExec)
+
+    assertEqual
+      "verify missing repo"
+      (Left MissingRepoDir)
+      (fst result)
+
+test_UninitializedGit :: Test
+test_UninitializedGit =
+  TestCase $ do
+    let mock = addMockExecResults results mockWithVaultAndRepoDir
+          where
+            results =
+              [ D.failedExecResult
+              ]
+    let result = runState (runExceptT verifyRepo) mock
+    let mockAfterExec = snd result
+
+    let expectedCommands =
+          [ ("dirExists", ["repo"]),
+            ("git", ["status"])
+          ]
+
+    assertEqualLists
+      "check repo commands"
+      expectedCommands
+      (execRecorded mockAfterExec)
+
+    assertEqual
+      "verify uninitialized git repo"
+      (Left UninitializedGit)
+      (fst result)
