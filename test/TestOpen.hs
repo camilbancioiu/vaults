b0a632ed48a8e501941ad5a819fb9ba177889bb3
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2025-07-01 22:05:28 +0300

    Refactor some tests

--- test/TestOpen.hs
+++ test/TestOpen.hs
@@ -17,180 +17,190 @@ import qualified Vaults.Substrate as Substrate
 allTests :: Test
 allTests =
   TestList
-    [ test_prerequisites,
-      test_openVault
+    [ test_openVault_without_partition_file,
+      test_openVault_in_non_vault_folder,
+      test_loopSetup_err_fails,
+      test_unlock_fails_undoes_loopSetup,
+      test_mount_fails_undoes_unlock_loopSetup,
+      test_mount_ok_no_inner_repo,
+      test_mount_ok_has_inner_repo
     ]
 
-test_prerequisites :: Test
-test_prerequisites =
-  TestList
-    [ TestLabel "open in non-vault folder fails" $
-        TestCase $ do
-          let mock = emptyMock
-          let result = runState (runExceptT $ openVault "local.vault") mock
-          let mockAfterExec = snd result
-          assertOpError "non-vault folder" result
-          assertNoExecCalls mockAfterExec,
-      TestLabel "open without a partition filename fails" $
-        TestCase $ do
-          let mock = mockWithVaultDir
-          let result = runState (runExceptT $ openVault "") mock
-          let mockAfterExec = snd result
-          assertOpError "partition filename is required" result
-          assertNoExecCalls mockAfterExec
-    ]
+test_openVault_without_partition_file :: Test
+test_openVault_without_partition_file =
+  TestCase $ do
+    let mock = mockWithVaultDir
+    let result = runState (runExceptT $ openVault "") mock
+    let mockAfterExec = snd result
+    assertOpError "partition filename is required" result
+    assertNoExecCalls mockAfterExec
 
-test_openVault :: Test
-test_openVault =
-  TestList
-    [ TestLabel "loop-setup error prevents opening" $
-        TestCase $ do
-          let mock = addMockExecResult result mockWithVaultDir
-                where
-                  result = D.loopSetupExec False D.localOp
-          let result = runState (runExceptT $ openVault "local.vault") mock
-          let mockAfterExec = snd result
+test_openVault_in_non_vault_folder :: Test
+test_openVault_in_non_vault_folder =
+  TestCase $ do
+    let mock = emptyMock
+    let result = runState (runExceptT $ openVault "local.vault") mock
+    let mockAfterExec = snd result
+    assertOpError "non-vault folder" result
+    assertNoExecCalls mockAfterExec
 
-          let failParams = snd $ D.loopSetupCmd D.localOp
-          let failExec = D.loopSetupExec False D.localOp
-          assertOpParamsError "loop-setup failed" failParams failExec result
+test_loopSetup_err_fails :: Test
+test_loopSetup_err_fails =
+  TestCase $ do
+    let mock = addMockExecResult result mockWithVaultDir
+          where
+            result = D.loopSetupExec False D.localOp
+    let result = runState (runExceptT $ openVault "local.vault") mock
+    let mockAfterExec = snd result
 
-          let expectedOperationCmds = [D.loopSetupCmd D.localOp]
-          let expectedCommands = D.preOpenPartitionCmds ++ expectedOperationCmds
+    let failParams = snd $ D.loopSetupCmd D.localOp
+    let failExec = D.loopSetupExec False D.localOp
+    assertOpParamsError "loop-setup failed" failParams failExec result
 
-          assertEqual
-            "only loop-setup was called"
-            expectedCommands
-            (execRecorded mockAfterExec)
-          assertEqual
-            "current directory not changed"
-            (currentDir mockWithVaultDir)
-            (currentDir mockAfterExec)
-          assertAllExecsConsumed mockAfterExec,
-      TestLabel "unlock error prevents opening and deletes loop device" $
-        TestCase $ do
-          let mock = addMockExecResults results mockWithVaultDir
-                where
-                  results =
-                    [ D.loopSetupExec True,
-                      D.unlockExec False,
-                      D.loopDeleteExec True
-                    ]
-                      <*> (pure D.localOp)
-          let result = runState (runExceptT $ openVault "local.vault") mock
-          let mockAfterExec = snd result
+    let expectedOperationCmds = [D.loopSetupCmd D.localOp]
+    let expectedCommands = D.preOpenPartitionCmds ++ expectedOperationCmds
 
-          let failParams = snd $ D.unlockCmd D.localOp
-          let failExec = D.unlockExec False D.localOp
-          assertOpParamsError "unlock failed" failParams failExec result
+    assertEqual
+      "only loop-setup was called"
+      expectedCommands
+      (execRecorded mockAfterExec)
+    assertEqual
+      "current directory not changed"
+      (currentDir mockWithVaultDir)
+      (currentDir mockAfterExec)
+    assertAllExecsConsumed mockAfterExec
 
-          let expectedOperationCmds =
-                [ D.loopSetupCmd,
-                  D.unlockCmd,
-                  D.loopDeleteCmd
+test_unlock_fails_undoes_loopSetup :: Test
+test_unlock_fails_undoes_loopSetup =
+  TestCase $
+    do
+      let mock = addMockExecResults results mockWithVaultDir
+            where
+              results =
+                [ D.loopSetupExec True,
+                  D.unlockExec False,
+                  D.loopDeleteExec True
                 ]
                   <*> (pure D.localOp)
-          let expectedCommands = D.preOpenPartitionCmds ++ expectedOperationCmds
+      let result = runState (runExceptT $ openVault "local.vault") mock
+      let mockAfterExec = snd result
 
-          assertEqual
-            "loop-setup, unlock, loop-delete were called"
-            expectedCommands
-            (execRecorded mockAfterExec)
-          assertEqual
-            "current directory not changed"
-            (currentDir mockWithVaultDir)
-            (currentDir mockAfterExec)
-          assertAllExecsConsumed mockAfterExec,
-      TestLabel "mount error prevents opening and undoes unlock and loop-setup" $
-        TestCase $ do
-          let mock = addMockExecResults results mockWithVaultDir
-                where
-                  results =
-                    [ D.loopSetupExec True,
-                      D.unlockExec True,
-                      D.mountExec False,
-                      D.lockExec True,
-                      D.loopDeleteExec True
-                    ]
-                      <*> (pure D.localOp)
-          let result = runState (runExceptT $ openVault "local.vault") mock
-          let mockAfterExec = snd result
+      let failParams = snd $ D.unlockCmd D.localOp
+      let failExec = D.unlockExec False D.localOp
+      assertOpParamsError "unlock failed" failParams failExec result
 
-          let failParams = snd $ D.mountCmd D.localOp
-          let failExec = D.mountExec False D.localOp
-          assertOpParamsError "mount failed" failParams failExec result
+      let expectedOperationCmds =
+            [ D.loopSetupCmd,
+              D.unlockCmd,
+              D.loopDeleteCmd
+            ]
+              <*> (pure D.localOp)
+      let expectedCommands = D.preOpenPartitionCmds ++ expectedOperationCmds
 
-          let expectedOperationCmds =
-                [ D.loopSetupCmd,
-                  D.unlockCmd,
-                  D.mountCmd,
-                  D.lockCmd,
-                  D.loopDeleteCmd
-                ]
-                  <*> (pure D.localOp)
-          let expectedCommands =
-                D.preOpenPartitionCmds
-                  ++ expectedOperationCmds
+      assertEqual
+        "loop-setup, unlock, loop-delete were called"
+        expectedCommands
+        (execRecorded mockAfterExec)
+      assertEqual
+        "current directory not changed"
+        (currentDir mockWithVaultDir)
+        (currentDir mockAfterExec)
+      assertAllExecsConsumed mockAfterExec
 
-          assertEqual
-            "loop-setup, unlock, mount, lock, loop-delete were called"
-            expectedCommands
-            (execRecorded mockAfterExec)
-          assertEqual
-            "current directory not changed"
-            (currentDir mockWithVaultDir)
-            (currentDir mockAfterExec)
-          assertAllExecsConsumed mockAfterExec,
-      TestLabel "mount succeeds, no inner repo" $
-        TestCase $ do
-          let mock = addMockExecResults results mockWithVaultDir
-                where
-                  results =
-                    [ D.loopSetupExec True,
-                      D.unlockExec True,
-                      D.mountExec True
-                    ]
-                      <*> (pure D.localOp)
-          let result = runState (runExceptT $ openVault "local.vault") mock
-          let mockAfterExec = snd result
+test_mount_fails_undoes_unlock_loopSetup :: Test
+test_mount_fails_undoes_unlock_loopSetup =
+  TestCase $ do
+    let mock = addMockExecResults results mockWithVaultDir
+          where
+            results =
+              [ D.loopSetupExec True,
+                D.unlockExec True,
+                D.mountExec False,
+                D.lockExec True,
+                D.loopDeleteExec True
+              ]
+                <*> (pure D.localOp)
+    let result = runState (runExceptT $ openVault "local.vault") mock
+    let mockAfterExec = snd result
 
-          let vri = D.makeVRI D.localOp ""
-          assertEqual "mount succeeds" (Right vri) (fst result)
+    let failParams = snd $ D.mountCmd D.localOp
+    let failExec = D.mountExec False D.localOp
+    assertOpParamsError "mount failed" failParams failExec result
 
-          let expectedCommands =
-                D.preOpenPartitionCmds
-                  ++ (D.openPartitionCmds D.localOp)
-                  ++ D.postOpenPartitionCmds D.localOp
+    let expectedOperationCmds =
+          [ D.loopSetupCmd,
+            D.unlockCmd,
+            D.mountCmd,
+            D.lockCmd,
+            D.loopDeleteCmd
+          ]
+            <*> (pure D.localOp)
+    let expectedCommands =
+          D.preOpenPartitionCmds
+            ++ expectedOperationCmds
 
-          assertEqual
-            "loop-setup, unlock, mount, lock, loop-delete were called"
-            expectedCommands
-            (execRecorded mockAfterExec)
-          assertAllExecsConsumed mockAfterExec,
-      TestLabel "mount succeeds, vault has inner repo" $
-        TestCase $ do
-          let mock = addMockExecResults results mockWithVaultAndRepoDir
-                where
-                  results =
-                    [ D.loopSetupExec True,
-                      D.unlockExec True,
-                      D.mountExec True
-                    ]
-                      <*> (pure D.localOp)
-          let result = runState (runExceptT $ openVault "local.vault") mock
-          let mockAfterExec = snd result
+    assertEqual
+      "loop-setup, unlock, mount, lock, loop-delete were called"
+      expectedCommands
+      (execRecorded mockAfterExec)
+    assertEqual
+      "current directory not changed"
+      (currentDir mockWithVaultDir)
+      (currentDir mockAfterExec)
+    assertAllExecsConsumed mockAfterExec
 
-          let vri = D.makeVRI D.localOp "/repo"
-          assertEqual "mount succeeds" (Right vri) (fst result)
+test_mount_ok_no_inner_repo :: Test
+test_mount_ok_no_inner_repo =
+  TestCase $ do
+    let mock = addMockExecResults results mockWithVaultDir
+          where
+            results =
+              [ D.loopSetupExec True,
+                D.unlockExec True,
+                D.mountExec True
+              ]
+                <*> (pure D.localOp)
+    let result = runState (runExceptT $ openVault "local.vault") mock
+    let mockAfterExec = snd result
 
-          let expectedCommands =
-                D.preOpenPartitionCmds
-                  ++ (D.openPartitionCmds D.localOp)
-                  ++ D.postOpenPartitionCmds D.localOp
+    let vri = D.makeVRI D.localOp ""
+    assertEqual "mount succeeds" (Right vri) (fst result)
 
-          assertEqual
-            "loop-setup, unlock, mount, lock, loop-delete were called"
-            expectedCommands
-            (execRecorded mockAfterExec)
-          assertAllExecsConsumed mockAfterExec
-    ]
+    let expectedCommands =
+          D.preOpenPartitionCmds
+            ++ (D.openPartitionCmds D.localOp)
+            ++ D.postOpenPartitionCmds D.localOp
+
+    assertEqual
+      "loop-setup, unlock, mount, lock, loop-delete were called"
+      expectedCommands
+      (execRecorded mockAfterExec)
+    assertAllExecsConsumed mockAfterExec
+
+test_mount_ok_has_inner_repo :: Test
+test_mount_ok_has_inner_repo =
+  TestCase $ do
+    let mock = addMockExecResults results mockWithVaultAndRepoDir
+          where
+            results =
+              [ D.loopSetupExec True,
+                D.unlockExec True,
+                D.mountExec True
+              ]
+                <*> (pure D.localOp)
+    let result = runState (runExceptT $ openVault "local.vault") mock
+    let mockAfterExec = snd result
+
+    let vri = D.makeVRI D.localOp "/repo"
+    assertEqual "mount succeeds" (Right vri) (fst result)
+
+    let expectedCommands =
+          D.preOpenPartitionCmds
+            ++ (D.openPartitionCmds D.localOp)
+            ++ D.postOpenPartitionCmds D.localOp
+
+    assertEqual
+      "loop-setup, unlock, mount, lock, loop-delete were called"
+      expectedCommands
+      (execRecorded mockAfterExec)
+    assertAllExecsConsumed mockAfterExec
